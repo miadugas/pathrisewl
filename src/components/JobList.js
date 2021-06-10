989f2245d9427567154781effe28c3345c8e9db3
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-06-10 16:25:13 -0600

    state factored

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,11 +1,20 @@
-import React, {useEffect, useState} from 'react';
-import CreateJob from '../modals/createJob';
+import React, { useState} from 'react';
+import CreateJob from '../modals/CreateJob';
 
 const JobList = () => {
     const [modal, setModal] = useState(false);
+    const [jobList, setJobList] = useState([]);
 
     const toggle = () => {
         setModal(!modal);
+    }
+
+    const saveJob = (jobObj) => {
+        let tempList = jobList
+        tempList.push(jobObj)
+    //     localStorage.setItem("jobList", JSON.stringify(tempList))
+        setJobList(tempList)
+        setModal(false)
     }
 
     return (
@@ -15,9 +24,10 @@ const JobList = () => {
             <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)}>Add a Job</button>
         </div>
         <div className = "job-container">
-
+        {jobList.map((obj) => <li>{obj.Name}</li> )}
+            {/* {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/> )} */}
         </div>
-        <CreateJob toggle = {toggle} modal = {modal}/>
+        <CreateJob toggle = {toggle} modal = {modal} save ={saveJob}/>
     </>
     );
 };
--- src/modals/createJob.js
+++ src/modals/createJob.js
@@ -1,36 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const createJob = ({modal,toggle}) => {
+const CreateJobPopup = ({modal,toggle, save}) => {
+    const [jobName, setJobName] = useState('');
+    const [jobTitle, setJobTitle] = useState('');
+    ////////////////////////////////////
+    // May add more fields
+    //
+    // const [webAddress, setWebAddress] = useState('');
+    //const [notes, setNotes] = useState('');
+    ////////////////////////////////////
+
+    const handleChange = (e) => {
+        
+        const {name, value} = e.target
+
+        if(name === "jobName"){
+        setJobName(value)
+        }else{
+            setJobTitle(value)
+        }
+
+    }
+
+    const handleSave = (e) => {
+    //     e.preventDefault()
+        let jobObj = {}
+        jobObj["Company"] = jobName
+        jobObj["Job Title"] = jobTitle
+        save(jobObj)
+    }
+
+
+    
     return (
         <Modal isOpen={modal} toggle={toggle} >
             <ModalHeader toggle={toggle}>Add a Job</ModalHeader>
             <ModalBody>
             <form>
-                <div className="form-group">
+                <div className="form-group" value = {jobName} onChange = {handleChange} name = "jobName">
                     <label>Company</label>
                     <input type="text" className = "form-control"/>
                 </div>
-                <div className="form-group">
+                <div className="form-group" value = {jobTitle} onChange = {handleChange} name = "jobTitle">
                 <label>Job Title</label>
                 <input type="text" className = "form-control"/>
                 </div>
-                <div className="form-group">
+                {/* <div className="form-group" value = {webAddress} onChange = {handleChange} name = "webAddress">
                 <label>Web Address</label>
                     <input type="text" className = "form-control"/>
                 </div>
-                <div className="form-group">
+                <div className="form-group" value = {notes} onChange = {handleChange} name = "notes">
                 <label>Notes</label>
                     <textarea rows="3" className = "form-control"/>
-                </div>
+                </div> */}
             </form>
             </ModalBody>
             <ModalFooter>
-                <Button color="primary" onClick={toggle}>Continue</Button>{' '}
+                <Button color="primary" onClick={handleSave}>Continue</Button>{' '}
                 <Button color="secondary" onClick={toggle}>Cancel</Button>
             </ModalFooter>
         </Modal>
     );
 };
 
-export default createJob;
+export default CreateJobPopup;
